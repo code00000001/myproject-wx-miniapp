ebf49493addedb272ae736a89a47af65b82c5d0a
Mivinci <1366723936@qq.com>
2019-04-02 16:04:34 +0800

    1st commit

---
+++ services/user.js
@@ -0,0 +1,5 @@
+/**
+ * Title: 我的页面的数据请求接口封装
+ * Author: xxx
+ * Date: xxxx-xx-xx xx:xx
+ */
---
+++ services/views.js
@@ -0,0 +1,20 @@
+/**
+ * Title: 看点页面的数据请求接口封装
+ * Author: XJJ
+ * Date: 2019-04-02 15:57
+ */
+
+import { request } from '../utils/request.js';
+
+const baseRoute = 'https://jsonplaceholder.typicode.com';
+
+const fetchTestData = params => {
+  return request(`${baseRoute}/todos`, {
+    method: 'GET',
+    body: params,
+  });
+}
+
+module.exports = {
+  fetchTestData,
+}
