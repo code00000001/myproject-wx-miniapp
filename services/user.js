785ed6dec79d4d3384edb70921ccad4111dd15a7
Mivinci <1366723936@qq.com>
2019-04-15 13:58:32 +0800

    Finish login service

--- services/user.js
+++ services/user.js
@@ -3,3 +3,22 @@
  * Author: xxx
  * Date: xxxx-xx-xx xx:xx
  */
+
+import { request } from '../utils/request.js';
+
+const baseRoute = 'http://maps.mapsong.com:2000/wap/user';
+
+
+// Login to get token and some userinfo 
+export const login = ({ 
+  code, 
+  appId, 
+  signature, 
+  rawData, 
+  encryptedData, 
+  iv 
+}) => {
+  return request(`${baseRoute}/login`, {
+    body: { code, appId, signature, rawData, encryptedData, iv, },
+  });
+};
