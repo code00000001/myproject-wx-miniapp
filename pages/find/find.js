27d1b14ae1b0cfd451ec39090a26a03a812db86c
Masaki <245551699@qq.com>
2019-04-29 22:37:03 +0800

    fk

--- pages/find/find.js
+++ pages/find/find.js
@@ -114,6 +114,8 @@ Page({
           createTime: date,
           src: res.tempFilePaths,
           srcImage:res.tempFilePaths[0]
+        },() => {
+          this.onShow()
         })
       },
     })
